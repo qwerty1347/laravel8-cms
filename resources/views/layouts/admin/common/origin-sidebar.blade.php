b7d402e9be8b104696ef6e0e0007eb0587855f1c
이건희 <qwertys1347@gmail.com>
2025-05-16 17:50:59 +0900

    style: 사이드 메뉴 바 관리

--- resources/views/layouts/admin/common/origin-sidebar.blade.php
+++ resources/views/layouts/admin/common/origin-sidebar.blade.php
@@ -15,7 +15,7 @@
           <svg class="nav-icon">
               <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-speedometer') }}"></use>
           </svg> Dashboard<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
-      {{-- <li class="nav-title">Theme</li>
+      <li class="nav-title">Theme</li>
       <li class="nav-item"><a class="nav-link" href="colors.html">
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-drop') }}"></use>
@@ -23,18 +23,34 @@
       <li class="nav-item"><a class="nav-link" href="typography.html">
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-pencil') }}"></use>
-          </svg> Typography</a></li> --}}
-      <li class="nav-title">관리</li>
-      <li class="nav-group {{ request()->is('admin/*') ? 'show' : '' }}"><a class="nav-link nav-group-toggle" href="#">
+          </svg> Typography</a></li>
+      <li class="nav-title">Components</li>
+      <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-puzzle') }}"></use>
-          </svg> 게시판 관리</a>
+          </svg> Base</a>
         <ul class="nav-group-items compact">
-          <li class="nav-item"><a class="nav-link {{ request()->is('admin/board/config*') ? 'active' : '' }}" href="base/accordion.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> 게시판 설정</a></li>
-          <li class="nav-item"><a class="nav-link {{ request()->is('admin/board/post*') ? 'active' : '' }} " href="base/breadcrumb.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> 게시판 목록</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/accordion.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Accordion</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/breadcrumb.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Breadcrumb</a></li>
+          <li class="nav-item"><a class="nav-link" href="https://coreui.io/bootstrap/docs/components/calendar/" target="_blank"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Calendar
+              <svg class="icon icon-sm ms-2">
+                <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-external-link') }}"></use>
+              </svg><span class="badge badge-sm bg-danger ms-auto">PRO</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="base/cards.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Cards</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/carousel.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Carousel</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/collapse.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Collapse</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/list-group.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> List group</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/navs-tabs.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Navs &amp; Tabs</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/pagination.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Pagination</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/placeholders.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Placeholders</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/popovers.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Popovers</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/progress.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Progress</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/spinners.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Spinners</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/tables.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Tables</a></li>
+          <li class="nav-item"><a class="nav-link" href="base/tooltips.html"><span class="nav-icon"><span class="nav-icon-bullet"></span></span> Tooltips</a></li>
         </ul>
       </li>
-      {{-- <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
+      <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-cursor') }}"></use>
           </svg> Buttons</a>
@@ -113,8 +129,8 @@
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-calculator') }}"></use>
           </svg> Widgets<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
-      <li class="nav-divider"></li> --}}
-      {{-- <li class="nav-title">Extras</li>
+      <li class="nav-divider"></li>
+      <li class="nav-title">Extras</li>
       <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
           <svg class="nav-icon">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-star') }}"></use>
@@ -145,7 +161,7 @@
       <li class="nav-item"><a class="nav-link text-primary fw-semibold" href="https://coreui.io/product/bootstrap-dashboard-template/" target="_top">
           <svg class="nav-icon text-primary">
             <use xlink:href="{{ asset('images/icons/sprites/free.svg#cil-layers') }}"></use>
-          </svg> Try CoreUI PRO</a></li> --}}
+          </svg> Try CoreUI PRO</a></li>
     </ul>
     <div class="sidebar-footer border-top d-none d-md-flex">
       <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
